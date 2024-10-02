393f27742fac573e7aff3771a1746adabab5401d
Mostafa  Fawzy <Moustafa_20210944@fci.helwan.edu.eg>
2024-10-02 14:48:58 +0300

    Remove node_modules and update .gitignore

--- myServer/models/category.js
+++ myServer/models/category.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 let categorySchema = mongoose.Schema({
   slug: String,
   name: String,
-  url: String
+  url: String,
+  description: String,
+  image: String
 });
 
 module.exports = mongoose.model("Category", categorySchema);
